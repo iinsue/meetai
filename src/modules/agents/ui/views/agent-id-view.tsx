34272ba63e2d4a6b6e11eb636ea4ea671edc6eb4
insu <hinsu93@gmail.com>
2025-08-10 23:23:09 +0900

    #15-2 Delete Agent

--- src/modules/agents/ui/views/agent-id-view.tsx
+++ src/modules/agents/ui/views/agent-id-view.tsx
@@ -1,13 +1,21 @@
 "use client";
 
+import { toast } from "sonner";
 import { useTRPC } from "@/trpc/client";
 import { VideoIcon } from "lucide-react";
-import { useSuspenseQuery } from "@tanstack/react-query";
+import { useRouter } from "next/navigation";
+import {
+  useMutation,
+  useQueryClient,
+  useSuspenseQuery,
+} from "@tanstack/react-query";
 
 import { Badge } from "@/components/ui/badge";
 import { ErrorState } from "@/components/error-state";
 import { LoadingState } from "@/components/loading-state";
 import { GeneratedAvatar } from "@/components/generated-avatar";
+
+import { useConfirm } from "@/hooks/use-confirm";
 
 import { AgentIdViewHeader } from "../components/agent-id-view-header";
 
@@ -17,47 +25,81 @@ interface Props {
 
 export const AgentIdView = ({ agentId }: Props) => {
   const trpc = useTRPC();
+  const router = useRouter();
+  const queryClient = useQueryClient();
 
   const { data } = useSuspenseQuery(
     trpc.agents.getOne.queryOptions({ id: agentId }),
   );
 
+  const removeAgent = useMutation(
+    trpc.agents.remove.mutationOptions({
+      onSuccess: async () => {
+        await queryClient.invalidateQueries(
+          trpc.agents.getMany.queryOptions({}),
+        );
+        // TODO: Invalidate free tier usage
+        router.push("/agents");
+      },
+      onError: (error) => {
+        toast.error(error.message);
+      },
+    }),
+  );
+
+  const [RemoveConfirmation, confirmRemove] = useConfirm(
+    "Are you sure?",
+    `The following action will remove ${data.meetingCount} associated meetings`,
+  );
+
+  const handleRemoveAgent = async () => {
+    const ok = await confirmRemove();
+
+    if (!ok) return;
+
+    await removeAgent.mutateAsync({ id: agentId });
+  };
+
   return (
-    <div className="flex flex-1 flex-col gap-y-4 p-4 md:px-8">
-      <AgentIdViewHeader
-        agentId={agentId}
-        agentName={data.name}
-        onEdit={() => {}}
-        onRemove={() => {}}
-      />
+    <>
+      <RemoveConfirmation />
 
-      <div className="rounded-lg border bg-white">
-        <div className="col-span-5 flex flex-col gap-y-5 px-4 py-5">
-          <div className="flex items-center gap-x-3">
-            <GeneratedAvatar
-              variant="botttsNeutral"
-              seed={data.name}
-              className="size-10"
-            />
-            <h2 className="text-2xl font-medium">{data.name}</h2>
-          </div>
+      <div className="flex flex-1 flex-col gap-y-4 p-4 md:px-8">
+        <AgentIdViewHeader
+          agentId={agentId}
+          agentName={data.name}
+          onEdit={() => {}}
+          onRemove={handleRemoveAgent}
+        />
 
-          <Badge
-            variant="outline"
-            className="flex items-center gap-x-2 [&_svg]:size-4"
-          >
-            <VideoIcon className="text-blue-700" />
-            {data.meetingCount}{" "}
-            {data.meetingCount === 1 ? "meeting" : "meetings"}
-          </Badge>
+        <div className="rounded-lg border bg-white">
+          <div className="col-span-5 flex flex-col gap-y-5 px-4 py-5">
+            <div className="flex items-center gap-x-3">
+              <GeneratedAvatar
+                variant="botttsNeutral"
+                seed={data.name}
+                className="size-10"
+              />
+              <h2 className="text-2xl font-medium">{data.name}</h2>
+            </div>
 
-          <div className="flex flex-col gap-y-4">
-            <p className="text-lg font-medium">Instructions</p>
-            <p className="text-neutral-800">{data.instructions}</p>
+            <Badge
+              variant="outline"
+              className="flex items-center gap-x-2 [&_svg]:size-4"
+            >
+              <VideoIcon className="text-blue-700" />
+              {data.meetingCount}{" "}
+              {data.meetingCount === 1 ? "meeting" : "meetings"}
+            </Badge>
+
+            <div className="flex flex-col gap-y-4">
+              <p className="text-lg font-medium">Instructions</p>
+              <p className="text-neutral-800">{data.instructions}</p>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
